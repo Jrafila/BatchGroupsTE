67863ce7ea624f041098734654337fada7772610
Jrafila <jrafila@sdl.com>
2024-12-12 22:23:12 +0000

    Update main_runner.py

--- main_runner.py
+++ main_runner.py
@@ -1,4 +1,6 @@
 import language_viewer
+import export_selected_to_excel  # Make sure export_selected_to_excel.py is in the same directory
 
 if __name__ == "__main__":
-    language_viewer.launch_ui()
+    language_viewer.launch_ui()  # Runs the GUI and blocks until the user finishes and the window closes
+    export_selected_to_excel.export_to_excel()  # Once done, generate the Excel file
