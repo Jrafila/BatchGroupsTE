c79f2c9d3e601bbfd1f24445fc74ff70123da283
Jrafila <jrafila@sdl.com>
2024-12-13 09:00:39 +0000

    Update main_runner.py

--- main_runner.py
+++ main_runner.py
@@ -3,4 +3,5 @@ import export_selected_to_excel  # Make sure export_selected_to_excel.py is in t
 
 if __name__ == "__main__":
     language_viewer.launch_ui()  # Runs the GUI and blocks until the user finishes and the window closes
+    export_selected_to_excel.get_customer_info() #Runs GUI for Location and Customer
     export_selected_to_excel.export_to_excel()  # Once done, generate the Excel file
